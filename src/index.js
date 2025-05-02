ba6cf727a867ce9287069ec66e4bc7f9295cdf2d
Riham Katout <rihamkatm@gmail.com>
2025-05-02 20:43:41 +0300

    Add router provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import routerConfig from "./Route";
+import { RouterProvider } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routerConfig} />
   </React.StrictMode>
 );
 
