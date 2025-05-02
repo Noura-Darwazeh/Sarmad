76615aa9953b96f33d70d5a5f8364133f48f72db
Riham Katout <rihamkatm@gmail.com>
2025-05-02 21:21:51 +0300

    Add font & Theme

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import routerConfig from "./Route";
 import { RouterProvider } from "react-router-dom";
+import { Theme } from "./utils/Theme";
+import { ThemeProvider } from "styled-components";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={routerConfig} />
+    <ThemeProvider theme={Theme}>
+      <RouterProvider router={routerConfig} />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
--- src/pages/MonthGamePage.js
+++ src/pages/MonthGamePage.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const MonthGamePage = () => {
   return (
-    <div>MonthGamePage</div>
+    <div>لعبة الشهر</div>
   )
 }
