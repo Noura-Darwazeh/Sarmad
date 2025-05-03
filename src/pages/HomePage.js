dcfc7ede550b1085bb5416ee880902a72e1d4041
Noura-Darwazeh <noura.darwazeh11@gmail.com>
2025-05-03 20:32:29 +0300

    navbar

---
+++ src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+// import Header from '../components/header'
+const HomePage = () => {
+  return (
+    <div>
+     <button>jjjj</button>
+     <p>kkkkkkkkkkk</p>
+     <p>kkkkkkkkkkk</p>
+
+     <p>kkkkkkkkkkk</p>
+     <p>kkkkkkkkkkk</p>
+     <p>kkkkkkkkkkk</p>
+     <p>kkkkkkkkkkk</p>
+     <p>kkkkkkkkkkk</p>
+     <p>kkkkkkkkkkk</p>
+     <p>kkkkkkkkkkk</p>
+     <p>kkkkkkkkkkk</p>
+     <p>kkkkkkkkkkk</p>
+     <p>kkkkkkkkkkk</p>
+
+    </div>
+  )
+}
+
+export default HomePage
